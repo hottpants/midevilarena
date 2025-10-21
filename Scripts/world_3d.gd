b478f8ad2111cecd46c7c1ee6e71ee804acc399a
hottpants <178601605+hottpants@users.noreply.github.com>
2025-10-21 15:23:01 -0700

    Ground

--- Scripts/world_3d.gd
+++ Scripts/world_3d.gd
@@ -1,15 +1,16 @@
 extends Node3D
-
+## penis
 const PLAYER = preload("res://Scenes/player.tscn")
 const ENEMY = preload("res://Scenes/enemy.tscn")
 
 var player
 var paused := false
+@onready var MENU = $"../HUD/Menu"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	$"../HUD/Menu".visible = false
+	MENU.visible = false
 	player = PLAYER.instantiate()
 	add_child(player)
 	
@@ -24,18 +25,18 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE: ## PAUSED
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-			$"../HUD/Menu".visible = false
+			MENU.visible = false
 			paused = false
 		else:                                            ## NOT PAUSED
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-			$"../HUD/Menu".visible = true
+			MENU.visible = true
 			paused = true
 
 
 func _on_resume_pressed() -> void:
 	if paused:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		$"../HUD/Menu".visible = false
+		MENU.visible = false
 		paused = false
 		player.paused = false
 		
